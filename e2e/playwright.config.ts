c68a533a127e26153d957c59bbe79fe3546a8481
marianozamora <mazachi93@gmail.com>
2026-05-15 23:45:38 -0500

    fix: set reuseExistingServer true in playwright config for docker-compose CI

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   webServer: {
     command: 'cd ../frontend && npm run dev',
     url: 'http://localhost:3000',
-    reuseExistingServer: !process.env['CI'],
+    reuseExistingServer: true,
     timeout: 30_000,
   },
 })
